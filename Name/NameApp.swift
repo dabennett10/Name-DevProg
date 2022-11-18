88bc18ca96e395df5f6b76708748fa67df889ba9
David Bennett <dabennett10@gmail.com>
2022-11-17 20:12:20 -0500

    Initial Commit

---
+++ Name/NameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameApp.swift
+//  Name
+//
+//  Created by David Bennett on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct NameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
